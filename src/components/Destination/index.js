3d1997fde98ec8256e949dd0d0aae6ae94ff01a6
alerafart <maalejandrarafart@gmail.com>
2022-10-20 17:26:15 +0200

    wip

--- src/components/Destination/index.js
+++ src/components/Destination/index.js
@@ -1,5 +1,6 @@
 //dependencies
-// import { useState } from 'react';
+import { useContext, createContext } from 'react';
+import { useState } from 'react';
 // import { NavLink, Link } from 'react-router-dom';
 // == Import component
 import DestinationNav from './DestinationNav';
@@ -7,50 +8,45 @@ import DestinationNav from './DestinationNav';
 import './destination.scss';
 //data
 import data from '../../data.js';
+const DestinationContext = createContext('Unknown');
+let destinations = data.destinations;
 // == Component
 function Destination() {
+  
   console.log(data.destinations);
   // console.log(data.destinations[0].name);
 
-  // const [destination, setDestination]= useState(data.destinations[0]);
-  // function  handleDestinantion(e) {
-  //   setDestination(e.target.value);
-  // }
-  // let destinations = data.destinations;
+  const [destination, setDestination]= useState(data.destinations[0]);
+  function  handleDestinantion(e) {
+    setDestination(e.target.value);
+  }
 
   // destinations.map((destination)=> {
   //   return destination.name;
   // })
   return (
-    <div className="destination">
-      <div className="destination__view">
-        <h5 className="destination__view__h5">
-          <pre>
-            <span className="span">01</span> Pick up your destination
-          </pre>
-        </h5>
+    <DestinationContext.Provider value={destinations}>
+      <div className="destination">
+        <div className="destination__view">
+          <h5 className="destination__view__h5">
+            <pre>
+              <span className="span">01</span> Pick up your destination
+            </pre>
+          </h5>
+        </div>
+        <section className="destination__info">
+          <DestinationNav value={destinations}/>  
+        
+        <h1 className="destination__info--name">
+          {data.destinations[0].name}
+        </h1>
+        <p className="destination__info--desc">
+          {data.destinations[0].description}
+        </p>
+        </section>
+        
       </div>
-      <section className="destination__info">
-        <DestinationNav />  
-      {/* <nav className="destination__info__nav">
-        {
-          destinations.map((destination)=> {
-            return <Link className="destination__info__nav--link" key={destination.name}>
-                      {destination.name}
-                  </Link>;
-          })
-          
-        }
-      </nav> */}
-      <h1 className="destination__info--name">
-        {data.destinations[0].name}
-      </h1>
-      <p className="destination__info--desc">
-        {data.destinations[0].description}
-      </p>
-      </section>
-      
-    </div>
+    </DestinationContext.Provider>
   );
 }
 
