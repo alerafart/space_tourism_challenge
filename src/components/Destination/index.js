89d9add706369068aeb328aee928b57b87375ef2
alerafart <maalejandrarafart@gmail.com>
2022-10-20 16:08:49 +0200

    beginning destinations static page

--- src/components/Destination/index.js
+++ src/components/Destination/index.js
@@ -2,12 +2,30 @@
 
 // == Import style
 import './destination.scss';
+//data
+import data from '../../data.js';
 // == Component
 function destination() {
-  
+  console.log(data.destinations[0]);
+  console.log(data.destinations[0].name);
   return (
     <div className="destination">
-
+      <div className="destination__view">
+        <h5 className="destination__view__h5">
+          <pre>
+            <span className="span">01</span> Pick up your destination
+          </pre>
+        </h5>
+      </div>
+      <section className="destination__info">
+      <h1 className="destination__info--name">
+        {data.destinations[0].name}
+      </h1>
+      <p className="destination__info--desc">
+        {data.destinations[0].description}
+      </p>
+      </section>
+      
     </div>
   );
 }
