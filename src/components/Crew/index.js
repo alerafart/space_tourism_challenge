a868ba3f6593cf96164ba0e7a8c576e5c28b8ce5
alerafart <maalejandrarafart@gmail.com>
2022-10-25 17:21:06 +0200

    desktop crew members pages

--- src/components/Crew/index.js
+++ src/components/Crew/index.js
@@ -12,31 +12,33 @@ function Crew({member, currentMember, crewMembers, setCrewMember}) {
             <em className="em">01</em>   Meet your Crew
           </pre>
         </h5>
-        <h4 className="crew__info__role">
-          {member.role}
-        </h4>
-        <h3 className="crew__info__name">
-        {member.name}
-        </h3>
-        <p className="crew__info__bio">
-        {member.bio} 
-        </p>
+        <div>
+          <h4 className="crew__info__role">
+            {member.role}
+          </h4>
+          <h3 className="crew__info__name">
+          {member.name}
+          </h3>
+          <p className="crew__info__bio">
+          {member.bio} 
+          </p>
+        </div>
+        
         {/* Slider icons to navigate through each crew Member onClick */}
         <div className="crew__info__slider">
           {
               crewMembers.map((m)=> {
               return <Link 
               className = {currentMember === m.name? 'crew__info__slider--link active': 'crew__info__slider--link' }
-              // className="crew__info__slider--link"
               key={m.name}
               onClick={()=> {setCrewMember(m.name)}}
               >
-                <SiHeadspace />
+                <SiHeadspace className='icon'/>
               </Link>
               })
           }
-        
       </div>
+
       </div>
       
       <div className='crew__img'>
